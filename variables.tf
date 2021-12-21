4a0bb15d3c518b4423219ae60f33a577acc9a3c5
Freeda Francis <93197553+Freeda-F@users.noreply.github.com>
2021-12-22 02:51:38 +0530

    variables.tf

---
+++ variables.tf
@@ -0,0 +1,33 @@
+variable "access_key" {
+  default = "access-key here"
+}
+
+variable "secret_key" {
+  default = "secret-key here"
+}
+
+variable "region" {
+    default = "ap-south-1"
+}
+
+variable "ami-id" {
+    default = "ami-052cef05d01020f1d"
+  
+}
+
+variable "type" {
+    default = "t2.micro"
+  
+}
+variable "project" {
+    default = "ipstack-app"
+}
+
+variable "vpc-id" {
+  default = "vpc-0eb0bd9ac0ac8d067"
+}
+
+variable "cert-arn" {
+    default = "arn:aws:acm:ap-south-"
+  
+}
